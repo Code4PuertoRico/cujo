4cc7f0f38fd617c63299df8af5c87aad1c417fd9
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-04-21 13:56:41 -0400

    Update smart settings app

--- apps/smart_settings/views.py
+++ apps/smart_settings/views.py
@@ -1,37 +1,39 @@
+from __future__ import absolute_import
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
 
-from common.utils import exists_with_famfam, return_type, encapsulate
+from common.utils import return_type, encapsulate
+from common.widgets import exists_with_famfam
 
-from smart_settings.api import settings
+from .api import settings, settings_list, namespace_list
 
 
 def setting_list(request):
-    new_settings = []
-    for namespace, sub_settings in settings.items():
-        for sub_setting in sub_settings:
-            if not sub_setting.get('hidden', False):
-                new_settings.append({
-                    'module': sub_setting['module'],
-                    'name': sub_setting['name'],
-                    'global_name': sub_setting['global_name'],
-                    'description': sub_setting.get('description', None),
-                    'exists': sub_setting.get('exists', False),
-                    'default': sub_setting['default'],
-                    })
     context = {
         'title': _(u'settings'),
-        'object_list': new_settings,
+        'object_list': [setting for setting in settings_list if setting.hidden==False],
         'hide_link': True,
         'hide_object': True,
         'extra_columns': [
-            {'name': _(u'name'), 'attribute': 'global_name'},
-            {'name': _(u'default'), 'attribute': encapsulate(lambda x: return_type(x['default']))},
-            {'name': _(u'value'), 'attribute': encapsulate(lambda x: return_type(getattr(x['module'], x['name'])))},
-            {'name': _(u'description'), 'attribute': 'description'},
-            {'name': _(u'exists'), 'attribute': encapsulate(lambda x: exists_with_famfam(getattr(x['module'], x['name'])) if x['exists'] else '')},
-        ]
+            {'name': _(u'name'), 'attribute': encapsulate(lambda x: mark_safe(u'<span style="font-weight: bold;">%s</span><br />%s' % (x.global_name, x.description)))},
+            {'name': _(u'default'), 'attribute': encapsulate(lambda x: return_type(x.default))},
+            {'name': _(u'value'), 'attribute': encapsulate(lambda x: mark_safe(u'<div class="nowrap">%s&nbsp;%s</div>' % (
+                    return_type(getattr(x.module, x.name)),
+                    exists_with_famfam(getattr(x.module, x.name)) if x.exists else ''
+                )))
+            },
+        ],
+        #'temporary_navigation_links': {
+        #    'sidebar': {
+        #        'links': links,
+        #        'upload_interactive': {
+        #            'links': links
+        #        }
+        #    }
+        #},        
     }
 
     return render_to_response('generic_list.html', context,
